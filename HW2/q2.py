edd3b591e1ef5c1226e9a38dc332b21d11c4721d
Jimyang0724 <jimyang890724@gmail.com>
2022-05-09 20:20:05 +0800

    [UPDATE] question2 complete

--- HW2/q2.py
+++ HW2/q2.py
@@ -13,66 +13,102 @@ def write_to_file(path, file):
 
 def get_pred_modes(frame, block_size=16):
     height, width = frame.shape[:2]
-    modes = {}
+    collage = np.zeros(frame.shape[:2], dtype=np.uint8)
+    modes = {0:-1}
+    collage[0:block_size, 0:block_size] = 0
     
-    for h in range(16, height, block_size):
-        for w in range(16, width, block_size):
+    
+    for h in range(0, height, block_size):
+        for w in range(0, width, block_size):
 
             SAD = np.inf
-            ref_frame = frame[h:h+block_size, w:w+block_size]
-            pred_frame = np.zeros((block_size, block_size), dtype=np.int16)
-
-            for mode in range(5):
-                if mode == 0:
-                    ref_value = frame[h-1, w:w+block_size]
-                    for i, val in zip(range(block_size), ref_value):
-                        pred_frame[:, i] = val
-                    SAD_local = np.sum(np.abs(np.subtract(ref_frame, pred_frame, dtype=np.int16)))
-                    if SAD_local <= SAD:
-                        SAD = SAD_local
+            mode_to_use = np.inf
+            ref_block = frame[h:h+block_size, w:w+block_size]
+            pred_block = np.zeros((block_size, block_size), dtype=np.int16)
+            
+            if h==0 and w==0:
+                continue
+            
+            elif h==0:
+                mode_to_use = 1
+                ref_value = frame[h:h+block_size, w-1]
+                for i, val in zip(range(block_size), ref_value):
+                    pred_block[i, :] = val
+                                    
+            elif w==0:
+                mode_to_use = 0
+                ref_value = frame[h-1, w:w+block_size]
+                for i, val in zip(range(block_size), ref_value):
+                    pred_block[:, i] = val
+                                    
+            else:
+                for mode in range(5):
+                    local_pred_block = np.zeros((block_size, block_size), dtype=np.int16)
                     
-                elif mode == 1:
-                    ref_value = frame[h:h+block_size, w-1]
-                    for i, val in zip(range(block_size), ref_value):
-                        pred_frame[i, :] = val
-                    SAD_local = np.sum(np.abs(np.subtract(ref_frame, pred_frame, dtype=np.int16)))
-                    if SAD_local <= SAD:
-                        SAD = SAD_local
-                
-                elif mode == 2:
-                    ref_value = np.mean(frame[h-1, w:w+block_size] + frame[h:h+block_size, w-1], dtype=np.int16)
-                    pred_frame[:] = ref_value
-                    SAD_local = np.sum(np.abs(np.subtract(ref_frame, pred_frame, dtype=np.int16)))
-                    if SAD_local <= SAD:
-                        SAD = SAD_local                 
+                    if mode == 0:
+                        ref_value = frame[h-1, w:w+block_size]
                         
-                elif mode == 3:
-                    continue
-                
-                elif mode == 4:
-                    ref_value = frame[h-1, w-1:w+block_size]
-                    ref_value = np.concatenate(([ref_value], [frame[h-1:h+block_size, w-1]]))
-                    for i in range(block_size):
-                        for j in range(block_size):
-                            pred_frame[i, j] = 
-                    # print(ref_value)
-                    # print(pred_frame)
-                    # print()
-
-                
-                else:
-                    continue
+                        for i, val in zip(range(block_size), ref_value):
+                            local_pred_block[:, i] = val
+                            
+                        SAD_local = np.sum(np.abs(np.subtract(ref_block, local_pred_block, dtype=np.int16)))
+                        if SAD_local <= SAD:
+                            SAD = SAD_local
+                            mode_to_use = mode
+                            pred_block = local_pred_block
+                        
+                    elif mode == 1:
+                        ref_value = frame[h:h+block_size, w-1]
+                        
+                        for i, val in zip(range(block_size), ref_value):
+                            local_pred_block[i, :] = val
+                            
+                        SAD_local = np.sum(np.abs(np.subtract(ref_block, local_pred_block, dtype=np.int16)))
+                        if SAD_local <= SAD:
+                            SAD = SAD_local
+                            mode_to_use = mode
+                            pred_block = local_pred_block
+                    
+                    elif mode == 2:
+                        ref_value = np.mean(frame[h-1, w:w+block_size] + frame[h:h+block_size, w-1], dtype=np.int16)
+                        local_pred_block[:] = ref_value
+                        SAD_local = np.sum(np.abs(np.subtract(ref_block, local_pred_block, dtype=np.int16)))
+                        if SAD_local <= SAD:
+                            SAD = SAD_local
+                            mode_to_use = mode
+                            pred_block = local_pred_block
+                            
+                    elif mode == 3:
+                        continue
+                    
+                    elif mode == 4:
+                        local_pred_block[0, :] = frame[h-1, w-1:w+block_size-1]
+                        local_pred_block[:, 0] = frame[h-1:h+block_size-1, w-1]
+                        
+                        for i in range(1, block_size):
+                            for j in range(1, block_size):
+                                local_pred_block[i, j] = local_pred_block[i - 1, j - 1]
+                                
+                        SAD_local = np.sum(np.abs(np.subtract(ref_block, local_pred_block, dtype=np.int16)))
+                        if SAD_local <= SAD:
+                            SAD = SAD_local
+                            mode_to_use = mode
+                            pred_block = local_pred_block
+                        
+                    else:
+                        print("Something Error!")
+                        break
                 
                 # SAD_local = np.sum(np.abs(np.subtract(frame[h:h+block_size, w:w+block_size], ref_frame_border[h+i:h+i+block_size, w+j:w+j+block_size], dtype=np.int16)))
                 # if SAD_local <= SAD:
                 #     SAD = SAD_local
                 #     min_vector = ((h + i - block_size) - h, (w + j - block_size) - w)
                             
-            modes[int((h / block_size) * (width / block_size) + (w / block_size))] = mode
-                    
-
+            modes[int((h / block_size) * (width / block_size) + (w / block_size))] = mode_to_use
+            collage[h:h+block_size, w:w+block_size] = pred_block
+            
     write_to_file(os.path.join(DST_FOLDER_TXT, "modes.txt"), modes)
-    return modes
+    return collage, modes
 
 def make_collage(ref_frame, MVs, block_size=16):
     height, width = ref_frame.shape[:2]
@@ -94,19 +130,13 @@ def make_collage(ref_frame, MVs, block_size=16):
     return pred_frame
     
 def main():
-    # ref_frame_bgr = cv2.imread(os.path.join(SRC_FOLDER, 'foreman_qcif_0_rgb.bmp'))
-    frame_bgr = cv2.imread(os.path.join(SRC_FOLDER, 'foreman_qcif_1_rgb.bmp'))
-    
-    # ref_frame_Y, _, _ = cv2.split(cv2.cvtColor(ref_frame_bgr, cv2.COLOR_BGR2YCrCb))
+    frame_bgr = cv2.imread(os.path.join(SRC_FOLDER, 'foreman_qcif_0_rgb.bmp'))
     frame_Y, _, _ = cv2.split(cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2YCrCb))
     
-    # cv2.imwrite(os.path.join(DST_FOLDER_IMG, 'ref_frame.png'), ref_frame_Y)
-    cv2.imwrite(os.path.join(DST_FOLDER_IMG, 'cur_frame.png'), frame_Y)
+    cv2.imwrite(os.path.join(DST_FOLDER_IMG, 'frame.png'), frame_Y)
 
-    modes = get_pred_modes(frame_Y)
-    # pred_frame = make_collage(ref_frame_Y, MVs)
-    
-    # cv2.imwrite(os.path.join(DST_FOLDER_IMG, 'pred_frame.png'), pred_frame)
+    collage, _ = get_pred_modes(frame_Y)    
+    cv2.imwrite(os.path.join(DST_FOLDER_IMG, 'pred_frame.png'), collage)
 
 if __name__ == '__main__':
     main()
